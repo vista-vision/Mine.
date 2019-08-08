528a0f0c9395a083275ff322c9900cf8f7fa05f0
Amit Beniwal <arunanshyadav@niser.ac.in>
2019-08-08 17:06:02 +0530

    Quite easier than I thought.

---
+++ q1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Please enter your name."
+read Name
+mkdir $Name
+touch $Name/$Name.cpp 
---
+++ q2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo 'Enter your name again.'
+read Name
+mkdir $Name
+touch $Name/$Name'1'.cpp
+touch $Name/$Name'2'.cpp
+touch $Name/$Name'3'.cpp
+touch $Name/$Name'4'.cpp
+touch $Name/$Name'5'.cpp
+
